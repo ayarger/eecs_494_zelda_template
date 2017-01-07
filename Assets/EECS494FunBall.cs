45398d7d3d8d7b9a1e4ba9bc9f630f89c07baeb7
Austin Yarger <ayarger@umich.edu>
2017-01-07 12:21:35 -0500

    Initial commit

---
+++ Assets/EECS494FunBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EECS494FunBallController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        // Lock ball to the x-y plane.
+        // This can be done far easier in the inspector (Rigidbody component).
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        ProcessScale();
+    }
+
+    /* Detect collision and increase size of ball */
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localScale = Vector3.one * 1.5f;
+    }
+
+    /* Ensure the ball return to normal size over time */
+    void ProcessScale()
+    {
+        if (transform.localScale.x > 1.0f)
+        {
+            transform.localScale -= Vector3.one * 0.1f;
+        }
+        else
+            transform.localScale = Vector3.one;
+    }
+}
---
+++ Assets/EECS494FunBallController.cs
@@ -0,0 +1,36 @@
+﻿/* A component controlling the behavior of the funnest ball around */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EECS494FunBall : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        // Lock ball to the x-y plane.
+        // This can be done far easier in the inspector (Rigidbody component).
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        ProcessScale();
+    }
+
+    /* Detect collision and increase size of ball */
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localScale = Vector3.one * 1.25f;
+    }
+
+    /* Ensure the ball return to normal size over time */
+    void ProcessScale()
+    {
+        if (transform.localScale.x > 1.0f)
+        {
+            transform.localScale -= Vector3.one * 0.01f;
+        }
+        else
+            transform.localScale = Vector3.one;
+    }
+}
